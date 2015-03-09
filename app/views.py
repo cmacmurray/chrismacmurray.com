579f809df1c470c3532c825941baa8c3b115922b
CMac <cmac@offerpop.com>
2015-03-09 11:19:21 -0400

    updating user model to include OpenID auth

--- app/views.py
+++ app/views.py
@@ -1,6 +1,8 @@
-from app import app
-from flask import render_template, flash, redirect
+from app import app, db, lm oid
+from flask import render_template, flash, redirect, session, url_for, request g
+from flask.ext import login_user, logout_user, current_user, login_required
 from .forms import LoginForm
+from .models import User
 
 @app.route('/')
 @app.route('/index')
@@ -20,13 +22,18 @@ def index():
                             title = 'HomePage',
                             user=user)
 @app.route('/login', methods=['GET','POST'])
+@oid.loginhandler
 def login():
+    if g.user is not None and g.user.is_authenticated():
+        return redirect(url_for('index'))
     form = LoginForm()
     if form.validate_on_submit():
-        flash('Login requestd for OpenID="%s", remember_me=%s' %
-              (form.openid.data, str(form.remember_me.data)))
-        return redirect('/index')
+        session['rmember_me'] form.remember_me.data
+        return oid.try_login(from.openid.data, ask_for=['nickname', 'email'])
     return render_template('login.html',
                             title='Sign In',
                             form = form,
                             providers=app.config['OPENID_PROVIDERS'])
+@lm.user_loader
+def load_user(id):
+    return User.query.get(int(id))
